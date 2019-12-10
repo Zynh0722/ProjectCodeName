97f0fc39cdbc50908f20baa1fccfe43f8a301bd3
Zynh0722 <Zynh0722@gmail.com>
2019-12-10 15:05:42 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='ProjectCodeName',
+      version='0.1',
+      description='',
+      author='Nicholas Ludwig, Ethan Hansen, Kaylani Kam, Nathan Gabrielsen',
+      packages=find_packages(),
+      zip_safe=False)
