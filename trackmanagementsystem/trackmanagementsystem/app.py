0a261f41e0786bddc3511bf64a95802d4641f351
Ethan Hansen <1ethanhansen@protonmail.com>
2020-01-15 09:00:40 -0800

    Starting basic app
    Updating app with new tutorial copypaste
    adding `pip install -e .` to user manual
    adding toga to requirements.txt

--- trackmanagementsystem/trackmanagementsystem/app.py
+++ trackmanagementsystem/trackmanagementsystem/app.py
@@ -1,27 +1,20 @@
-"""
-Gather Store and Track driver data, and win loss records
-"""
 import toga
-from toga.style import Pack
-from toga.style.pack import COLUMN, ROW
 
 
-class TrackManagementSystem(toga.App):
+def button_handler(widget):
+    print("hello")
 
-    def startup(self):
-        """
-        Construct and show the Toga application.
 
-        Usually, you would add your application to a main content box.
-        We then create a main window (with a name matching the app), and
-        show the main window.
-        """
-        main_box = toga.Box()
+def build(app):
+    box = toga.Box()
 
-        self.main_window = toga.MainWindow(title=self.name)
-        self.main_window.content = main_box
-        self.main_window.show()
+    button = toga.Button('Hello world', on_press=button_handler)
+    button.style.padding = 50
+    button.style.flex = 1
+    box.add(button)
+
+    return box
 
 
 def main():
-    return TrackManagementSystem('Track Management System', 'com.projectcodename.tms.trackmanagementsystem')
+    return toga.App('First App', 'org.beeware.helloworld', startup=build)
