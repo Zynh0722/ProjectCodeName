50d7109ec547d44604d91e8c19ede4e785212866
Ethan Hansen <1ethanhansen@protonmail.com>
2020-01-17 10:50:31 -0800

    Massive Refactor

--- setup.py
+++ setup.py
@@ -1,9 +1,78 @@
-from setuptools import setup
-from setuptools import find_packages
+#!/usr/bin/env python
+import io
+import re
+from setuptools import setup, find_packages
+import sys
 
-setup(name='ProjectCodeName',
-      version='0.1',
-      description='',
-      author='Nicholas Ludwig, Ethan Hansen, Kaylani Kam, Nathan Gabrielsen',
-      packages=find_packages(),
-      zip_safe=False)
+with io.open('./trackmanagementsystem/__init__.py', encoding='utf8') as version_file:
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file.read(), re.M)
+    if version_match:
+        version = version_match.group(1)
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name='trackmanagementsystem',
+    version=version,
+    description='Gather Store and Track driver data, and win loss records',
+    author='Nicholas Ludwig, Ethan Hansen, Kaylani Kam, Nathan Gabrielsen',
+    author_email='Zynh0722@gmail.com',
+    license='MIT license',
+    packages=find_packages(
+        exclude=[
+            'docs', 'tests',
+            'windows', 'macOS', 'linux',
+            'iOS', 'android',
+            'django'
+        ]
+    ),
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: MIT license',
+    ],
+    install_requires=[
+    ],
+    options={
+        'app': {
+            'formal_name': 'Track Management System',
+            'bundle': 'com.projectcodename.tms'
+        },
+
+        # Desktop/laptop deployments
+        'macos': {
+            'app_requires': [
+                'toga-cocoa==0.3.0.dev15',
+            ]
+        },
+        'linux': {
+            'app_requires': [
+                'toga-gtk==0.3.0.dev15',
+            ]
+        },
+        'windows': {
+            'app_requires': [
+                'toga-winforms==0.3.0.dev15',
+            ]
+        },
+
+        # Mobile deployments
+        'ios': {
+            'app_requires': [
+                'toga-ios==0.3.0.dev15',
+            ]
+        },
+        'android': {
+            'app_requires': [
+                'toga-android==0.3.0.dev15',
+            ]
+        },
+
+        # Web deployments
+        'django': {
+            'app_requires': [
+                'toga-django==0.3.0.dev15',
+            ]
+        },
+    }
+)
