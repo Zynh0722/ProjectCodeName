84d61ceaec55237169cc4835f199eacd0b48d07a
Ethan Hansen <1ethanhansen@protonmail.com>
2020-01-24 09:24:13 -0800

    will this work?

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,5 +1,4 @@
 import pytest
-import trackmanagementsystem.app as app
 import trackmanagementsystem.main_page_funcs as main_funcs
 
 
@@ -7,4 +6,4 @@ def test_travis_1():
     assert (True is True)
 
 def test_hello_there_1():
-    assert (main_funcs.hello_there() == True)
+    assert (main_funcs.hello_there() is True)
