303654ec029b9f017f6f3fe70e879e5062156078
Ethan Hansen <1ethanhansen@protonmail.com>
2020-01-28 10:47:16 -0800

    Creating basic vehicle class

---
+++ objects/vehicle.py
@@ -0,0 +1,8 @@
+class Vehicle:
+    def __init__(self, make_model, year, color, engine, name, affiliations):
+        self.make_model = make_model
+        self.year = year
+        self.color = color
+        self.engine = engine
+        self.name = name
+        self.affiliations = affiliations
--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,6 +1,13 @@
 import pytest
 import trackmanagementsystem.app as app
+import objects.vehicle
 
 
 def test_travis_1():
     assert (True is True)
+
+
+def test_new_vehicle():
+    vehicle_test = objects.Vehicle("DeLorean", 1985, "Space grey", "Flux capacity", "Timey Wimey",
+                                   "Amblin Entertainment")
+    assert (vehicle_test.name == "Timey Wimey")
