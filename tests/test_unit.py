535fd9e4fa4f31c237c3b4ce299e448cf13b5921
Ethan Hansen <1ethanhansen@protonmail.com>
2020-02-06 09:22:44 -0800

    fixing travis

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,10 +1,18 @@
 import pytest
-# from trackmanagementsystem.main_page_funcs import hello_there
+from trackmanagementsystem.main_page_funcs import black_delorean, red_ford, blue_toyota
 
 
 def test_travis_1():
     assert (True is True)
 
 
-def test_hello_there_1():
-    assert (hello_there() is True)
+def test_delorean_1():
+    assert (black_delorean() is True)
+
+
+def test_red_ford_1():
+    assert (red_ford() is True)
+
+
+def test_blue_toyota_1():
+    assert  (blue_toyota() is True)
