94499d752699c89c1e3210b62c85c073d53dcdb4
Ethan Hansen <1ethanhansen@protonmail.com>
2020-02-06 17:47:33 +0000

    Fixes #21 and fixes #52 and fixes #64 - Creating classes
    * Creating basic vehicle class
    
    * vehicle test should work
    
    * Added driver class
    
    * Split affiliations into sponsors and clubs

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,6 +1,21 @@
 import pytest
 import trackmanagementsystem.app as app
+from objects.vehicle import Vehicle
+from objects.driver import Driver
 
 
 def test_travis_1():
     assert (True is True)
+
+
+def test_new_vehicle():
+    vehicle_test = Vehicle("DeLorean", 1985, "Space grey", "Flux capacitor", "Timey Wimey",
+                           "Amblin Entertainment", "Time Travelers Anonymous")
+    assert (vehicle_test.name == "Timey Wimey" and vehicle_test.engine == "Flux capacitor")
+
+
+def test_new_driver():
+    driver_test = Driver("Marty McFly", "9303 Lyon Drive", "Hill Valley", "CA", 95420, 9168423138,
+                         "marty.mcfly@protonmail.com")
+
+    assert (driver_test.zip_code == 95420 and driver_test.state == "CA")
