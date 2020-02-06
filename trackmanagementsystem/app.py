c48673d0a446fb09a81398a39b96fc8d56a15cda
KayKam04 <58743881+KayKam04@users.noreply.github.com>
2020-02-06 08:30:02 -0800

    Changing Button Names and Outputs
    I made up three vehicle possibilities and have changed the Output to describe what vehicle it is and not General Kenobi
    
    Co-Authored-By: Ethan Hansen <1ethanhansen@protonmail.com>

--- trackmanagementsystem/app.py
+++ trackmanagementsystem/app.py
@@ -1,23 +1,64 @@
 import toga
-import main_page_funcs
+import trackmanagementsystem.main_page_funcs as main_page_funcs
+from toga.style.pack import Pack, COLUMN
 
 
-def hello_there_handler(widget):
-    main_page_funcs.hello_there()
+def red_ford_handler(widget):
+    main_page_funcs.red_ford()
+
+
+def blue_toyota_handler(widget):
+    main_page_funcs.blue_toyota()
+
+
+def black_delorean_handler(widget):
+    main_page_funcs.black_delorean()
 
 
 def build(app):
     box = toga.Box()
+    #
+    # data = [
+    #     ('root%s' % i, 'value %s' % i)
+    #     for i in range(1, 100)
+    # ]
+    #
+    # left_container = toga.Table(headings=['Red Ford'], data=data)
+    #
+    # right_content = toga.Box(
+    #     style=Pack(direction=COLUMN, padding_top=50)
+    # )
+    #
+    # for b in range(0, 10):
+    #     right_content.add(
+    #         toga.Button(
+    #             'Red Ford' % b,
+    #             on_press=red_ford_handler,
+    #             style=Pack(width=200, padding=20)
+    #         )
+    #     )
+    # right_container = toga.ScrollContainer(horizontal=False)
+    #
+    # right_container.content = right_content
+    #
+    # split = toga.SplitContainer()
+    #
+    # split.content = [left_container, right_container]
 
-    hello_there_button = toga.Button('Hello there', on_press=hello_there_handler)
-    hello_there_button.style.padding = 50
-    hello_there_button.style.flex = 1
-    box.add(hello_there_button)
+    red_ford_button = toga.Button('Red Ford', on_press=red_ford_handler)
+    red_ford_button.style.padding = 50
+    red_ford_button.style.flex = 1
+    box.add(red_ford_button)
 
-    goodbye_there_button = toga.Button('Goodbye', on_press=hello_there_handler)
-    goodbye_there_button.style.flex = 1
-    goodbye_there_button.style.padding = 50
-    box.add(goodbye_there_button)
+    blue_toyota_button = toga.Button('Blue Toyota', on_press=blue_toyota_handler)
+    blue_toyota_button.style.flex = 1
+    blue_toyota_button.style.padding = 50
+    box.add(blue_toyota_button)
+
+    black_delorean_button = toga.Button('Black DeLorean', on_press=black_delorean_handler)
+    black_delorean_button.style.padding = 50
+    black_delorean_button.style.flex = 1
+    box.add(black_delorean_button)
 
     return box
 
--- trackmanagementsystem/main_page_funcs.py
+++ trackmanagementsystem/main_page_funcs.py
@@ -1,3 +1,11 @@
-def hello_there():
-    print("General Kenobi")
+def red_ford():
+    print("Red Ford")
+    return True
+
+def blue_toyota():
+    print("Blue Toyota")
+    return True
+
+def black_delorean():
+    print("Black DeLorean")
     return True
