cc732b1ddb1da1361c7248ab657d51d4668a9e40
Ethan Hansen <1ethanhansen@protonmail.com>
2020-02-12 09:10:18 -0800

    drivers can be equal too i guess

--- objects/driver.py
+++ objects/driver.py
@@ -7,3 +7,12 @@ class Driver:
         self.zip_code = zip_code
         self.phone = phone
         self.email = email
+
+    def __eq__(self, other):
+        if not isinstance(other, Driver):
+            # don't attempt to compare against unrelated types
+            return NotImplemented
+
+        return self.name == other.name and self.address == other.address and \
+               self.city == other.city and self.state == other.state and self.zip_code == other.zip_code \
+               and self.phone == other.phone and self.email == other.email
